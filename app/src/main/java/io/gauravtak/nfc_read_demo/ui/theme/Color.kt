0c124306fc1a8f11c98ae36c617d2f888c5c1907
Gaurav Tak <gaurav.tak@roro.io>
2023-09-01 00:52:32 +0530

    Initial Commit

---
+++ app/src/main/java/io/gauravtak/nfc_read_demo/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package io.gauravtak.nfc_read_demo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val Blue = Color(0xFF007aff)
+val Gray = Color(0xFFaaaaaa)
+val White = Color(0xFFFFFFFF)
